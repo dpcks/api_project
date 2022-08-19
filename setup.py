076c34a1c2d53e96ba9f80cd173f0316e37d4e14
lyc <hogus5959@gmail.com>
2022-08-19 12:39:39 +0900

    add: setup

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ setup(
     name = "myapi", # 이 이름으로 패키지가 설치
     version = "0.0.1", # 릴리즈 넘버.메이저번호.마이너번호
     description = "my api lib",
-    url = "https://github.com/dpcks/api_project.git"
+    url = "https://github.com/dpcks/api_project.git",
     author = "yechany",
     author_email = "jya05064@naver.com",
     packages= ["my_api"],
     install_requires = [
-        "request"
+        "requests"
     ]
 )
